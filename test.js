6f163cf48b93b16e76e9cadc6aedd57b06b9346a
David Björklund <david.bjorklund@gmail.com>
2016-05-10 22:28:41 +0200

    more tests

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ test('setIn with unchanged array', t => {
     a: 'b',
     c: [1, 2, 3]
   });
-  const actual = setIn(input, ['a'], 'yo');
+  const actual = setIn(input, Object.freeze(['a']), 'yo');
   const expected = {
     a: 'yo',
     c: [1, 2, 3]
@@ -35,7 +35,22 @@ test('setIn with unchanged value', t => {
       b: 'c'
     }
   });
-  const actual = setIn(input, ['a', 'b'], 'c');
+  const actual = setIn(input, Object.freeze(['a', 'b']), 'c');
   const expected = input;
   t.is(actual, expected);
+});
+
+test('setIn with nested changed value', t => {
+  const input = Object.freeze({
+    a: {
+      b: 'c'
+    }
+  });
+  const actual = setIn(input, Object.freeze(['a', 'b']), 'd');
+  const expected = {
+    a: {
+      b: 'd'
+    }
+  };
+  t.deepEqual(actual, expected);
 });
